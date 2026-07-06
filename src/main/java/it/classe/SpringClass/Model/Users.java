1ce78e3eb897e7281ad82921faa314f77e46f7ce
Vincenzo <Vincenzo_piccolo@outlook.it>
2026-07-06 17:21:27 +0200

    add .~lock.Esercizitazioni Java Lis Map Hash.docx# Collections_Framework_e_Tipi_Generici_Dettagliato.pptx     Design Pattern.docx     Esercizitazioni Java Lis Map Hash.docx     Git_GitHub_Slide.pptx     Introduzione_a_Mockito.pptx     Java base - blocco 1 - Introduzione.pptx     Java base - blocco 2 - La struttura.pptx     Java base - blocco 4 - meccanismi di importazione.pptx     Java base - blocco 5 - gli errori e le eccezioni.pptx     Java base - blocco 6 - concetti fondamentali di OOP.pptx     Java base - blocco 7 - Ereditarietà.pptx     Java base - blocco 8 - Gli Array, Vector, Array list.pptx     Java base - blocco 9 - Database.pptx     JUnit_Presentazione.pptx     Microservizi_Spring_Boot.pptx     principi_SOLID.pptx     SLF4J_Guida_Esempi.pptx     spring.docx     Spring_Profiles.pptx     Spring_Security_Presentation_v2.pptx     Stream.txt     Users.java     ~$ercizitazioni Java Lis Map Hash.docx  modify application.properties

---
+++ src/main/java/it/classe/SpringClass/Model/Users.java
@@ -0,0 +1,29 @@
+package it.classe.SpringClass.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Table(name = "users", schema = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String username;
+    private String password;
+    private String email;
+
+
+
+}
