2d7622afef19594d4722a00b2af1ec92f8f6cf8c
Vincenzo <Vincenzo_piccolo@outlook.it>
2026-07-06 15:51:38 +0200

    First commir

---
+++ src/main/java/it/classe/SpringClass/SpringClassApplication.java
@@ -0,0 +1,13 @@
+package it.classe.SpringClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClassApplication.class, args);
+	}
+
+}
