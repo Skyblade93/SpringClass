4fd2d0bc420955c5b89e164f644d3ea3e48d0962
AlessandroSottosanti <alessandro.sottosanti@koinvest.it>
2026-07-07 16:38:18 +0200

    refactor: PaymentMapper

--- src/main/java/it/classe/SpringClass/Mapper/PaymentMapper.java
+++ src/main/java/it/classe/SpringClass/Mapper/PaymentMapper.java
@@ -1,53 +1,20 @@
 package it.classe.SpringClass.Mapper;
 
-import it.classe.SpringClass.Dto.AlunnoDto;
 import it.classe.SpringClass.Dto.PaymentDto;
-import it.classe.SpringClass.Model.Alunno;
 import it.classe.SpringClass.Model.Payment;
+import org.modelmapper.ModelMapper;
 
-import java.util.ArrayList;
-import java.util.List;
+public class PaymentMapper extends AbstractConverter<Payment, PaymentDto> {
 
-public class PaymentMapper {
-    public static PaymentDto toDto(Payment entity){
-        PaymentDto dto = new PaymentDto();
-        dto.setId(entity.getId());
-        dto.setStatus(entity.getStatus());
-        dto.setPaymentMethod(entity.getPaymentMethod());
-        dto.setAmount(entity.getAmount());
-        dto.setProcessedAt(entity.getProcessedAt());
-        dto.setPayer(UsersMapper.toDto(entity.getPayer()));
-        dto.setPayee(UsersMapper.toDto(entity.getPayee()));
+    ModelMapper mapper = new ModelMapper();
 
-        return dto;
+    @Override
+    public Payment toEntity(PaymentDto paymentDto) {
+        return mapper.map(paymentDto, Payment.class);
     }
 
-    public static Payment toEntity(PaymentDto dto){
-        Payment entity = new Payment();
-        entity.setId(dto.getId());
-        entity.setStatus(dto.getStatus());
-        entity.setPaymentMethod(entity.getPaymentMethod());
-        entity.setAmount(dto.getAmount());
-        entity.setProcessedAt(dto.getProcessedAt());
-        entity.setPayer(UsersMapper.toEntity(dto.getPayer()));
-        entity.setPayee(UsersMapper.toEntity(dto.getPayee()));
-
-        return entity;
-    }
-
-    public static List<Payment> toEntityList(List<PaymentDto> dtos) {
-        List<Payment> list = new ArrayList<>();
-        for (PaymentDto dto : dtos) {
-            list.add(toEntity(dto));
-        }
-        return list;
-    }
-
-    public static List<PaymentDto> toDtoList(List<Payment> entity) {
-        List<PaymentDto> listdto = new ArrayList<>();
-        for (Payment dto : entity) {
-            listdto.add(toDto(dto));
-        }
-        return listdto;
+    @Override
+    public PaymentDto toDTO(Payment payment) {
+        return mapper.map(payment, PaymentDto.class);
     }
 }
