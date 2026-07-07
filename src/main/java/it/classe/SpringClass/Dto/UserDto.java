01d8adb06c6d0478a85aa3a3cb22cfd5482b0c89
Vincenzo <Vincenzo_piccolo@outlook.it>
2026-07-07 15:03:55 +0200

    Create UserDto

---
+++ src/main/java/it/classe/SpringClass/Dto/UserDto.java
@@ -0,0 +1,26 @@
+package it.classe.SpringClass.Dto;
+
+import it.classe.SpringClass.Model.Auto;
+import it.classe.SpringClass.Model.Cards;
+import it.classe.SpringClass.Model.Payment;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class UserDto {
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+    private List<Cards> cards;
+    private List<Auto> auto_possedute ;
+    private List<Payment> sentPayments ;
+    private List<Payment> receivedPayments ;
+}
