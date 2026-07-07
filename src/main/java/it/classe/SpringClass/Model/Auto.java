ef87bf1ff47092fb16ded78aa5e84df30cf15fd5
vale_ <vale_bagnuolo@hotmail.it>
2026-07-07 10:33:29 +0200

    Aggiungo auto

---
+++ src/main/java/it/classe/SpringClass/Model/Auto.java
@@ -0,0 +1,27 @@
+package it.classe.SpringClass.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+@Table(name = "cars", schema = "cars")
+public class Auto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer anno;
+    private String marca;
+    private String modello;
+    private String colore;
+}
