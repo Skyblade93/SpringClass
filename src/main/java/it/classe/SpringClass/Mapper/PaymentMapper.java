7e152932328df76e4d8825765aa59b727cabae7b
AlessandroSottosanti <alessandro.sottosanti@koinvest.it>
2026-07-07 16:53:05 +0200

    refactor: PaymentMapper

--- src/main/java/it/classe/SpringClass/Mapper/PaymentMapper.java
+++ src/main/java/it/classe/SpringClass/Mapper/PaymentMapper.java
@@ -9,12 +9,12 @@ public class PaymentMapper extends AbstractConverter<Payment, PaymentDto> {
     ModelMapper mapper = new ModelMapper();
 
     @Override
-    public Payment toEntity(PaymentDto paymentDto) {
-        return mapper.map(paymentDto, Payment.class);
+    public Payment toEntity(PaymentDto dto) {
+        return mapper.map(dto, Payment.class);
     }
 
     @Override
-    public PaymentDto toDTO(Payment payment) {
-        return mapper.map(payment, PaymentDto.class);
+    public PaymentDto toDTO(Payment entity) {
+        return mapper.map(entity, PaymentDto.class);
     }
 }
