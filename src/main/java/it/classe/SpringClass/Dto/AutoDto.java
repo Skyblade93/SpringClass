cc26be9eeaa5907fcab27eb5228870e81e6a6bab
vale_ <vale_bagnuolo@hotmail.it>
2026-07-07 15:27:12 +0200

    AutoDto

---
+++ src/main/java/it/classe/SpringClass/Dto/AutoDto.java
@@ -0,0 +1,23 @@
+package it.classe.SpringClass.Dto;
+
+import it.classe.SpringClass.Model.Users;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class AutoDto {
+        private Integer id;
+        private Integer anno;
+        private String marca;
+        private String modello;
+        private String colore;
+
+}
+
