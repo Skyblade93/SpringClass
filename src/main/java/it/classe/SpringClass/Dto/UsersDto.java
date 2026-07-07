d69cd280afd241e6f963a356fe0e8828df7935ef
Vincenzo <Vincenzo_piccolo@outlook.it>
2026-07-07 15:12:17 +0200

    modify CardsDto.java        UsersDto.java

---
+++ src/main/java/it/classe/SpringClass/Dto/UsersDto.java
@@ -0,0 +1,22 @@
+package it.classe.SpringClass.Dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class UsersDto {
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+    private List<CardsDto> cards;
+    private List<AutoDto> auto_possedute ;
+    private List<PaymentDto> sentPayments ;
+    private List<PaymentDto> receivedPayments ;
+}
