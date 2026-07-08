7aa53d32e95d149f005ddbcecb7147298d6dd3c2
Nicola <n.pignatiello@itdm.it>
2026-07-08 14:09:07 +0200

    Implementazione, miglioria ServiceContoCorrente

--- src/main/java/it/classe/SpringClass/Service/ContoCorrenteService.java
+++ src/main/java/it/classe/SpringClass/Service/ContoCorrenteService.java
@@ -1,14 +1,10 @@
 package it.classe.SpringClass.Service;
 
 import it.classe.SpringClass.Dto.ContoCorrenteDto;
-import it.classe.SpringClass.Dto.UsersDto;
 import it.classe.SpringClass.Mapper.ContoCorrenteMapper;
 import it.classe.SpringClass.Mapper.Converter;
-import it.classe.SpringClass.Mapper.UsersMapper;
 import it.classe.SpringClass.Model.ContoCorrente;
-import it.classe.SpringClass.Model.Users;
 import it.classe.SpringClass.Repository.ContoCorrenteRepository;
-import it.classe.SpringClass.Repository.UsersRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -32,9 +28,23 @@ public class ContoCorrenteService extends AbstractService<ContoCorrente ,ContoCo
             this.contoCorrenteRepository=contoCorrenteRepository;
     }
 
-        public ContoCorrenteDto findByIdConto(Integer idonto){
-
-            return contoCorrenteMapper.toDTO(contoCorrenteRepository.findById(idonto));
+        public ContoCorrenteDto findByIdConto(Integer idConto){
+            return contoCorrenteMapper.toDTO(contoCorrenteRepository.findById(idConto));
         }
+
+            public ContoCorrenteDto findByName(String nome){
+        return contoCorrenteMapper.toDTO(contoCorrenteRepository.findByName(nome));
+
+            }
+
+            public ContoCorrenteDto findByEmail(String email)
+            {
+                return contoCorrenteMapper.toDTO(contoCorrenteRepository.findByEmail(email));
+            }
+            public ContoCorrenteDto findByCognome(String cognome){
+        return contoCorrenteMapper.toDTO(contoCorrenteRepository.findByEmail(cognome));
+            }
+
+
 
 }
