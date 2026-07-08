9dcf1c15c386570247a1506d1a3bc65ea3e6e62a
Francesco Pio Cataudo <164793263+fcataudo1@users.noreply.github.com>
2026-07-08 11:57:11 +0200

    modify OrdineRepository

--- src/main/java/it/classe/SpringClass/Repository/OrdineRepository.java
+++ src/main/java/it/classe/SpringClass/Repository/OrdineRepository.java
@@ -1,28 +1,30 @@
 package it.classe.SpringClass.Repository;
 
 import it.classe.SpringClass.Model.Ordine;
+import it.classe.SpringClass.Model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface OrdineRepository extends JpaRepository<Ordine, Integer> {
 
-    Ordine findByProdotti(String prodotti);
-    Ordine findByDatacaCreazione(LocalDate dataCreazione);
-    Ordine findByImporto(double importo);
-    Ordine findByQuantita(int quantita);
+    Ordine findByDataCreazione(LocalDateTime dataCreazione);
+    List<Ordine> findByImporto(double importo);
+    List<Ordine> findByQuantita(int quantita);
+    List<Ordine> findByUsers(Users users);
 
     @Query("SELECT o FROM Ordine o WHERE o.prodotti = ?1")
-    Ordine prodotti(String prodotti);
+    List<Ordine> cercaProdotti(String prodotti);
 
-    @Query("SELECT o FROM Ordine o WHERE o.importo = ?1")
-    Ordine importo(double importo);
+    @Query("SELECT o FROM Ordine o ORDER BY o.importo DESC")
+    List<Ordine> ordiniDalPiuCostoso();
 
-    @Query(value = "SELECT * FROM Ordine o WHERE o.prodotti = ?1",nativeQuery = true)
-    Ordine prodottiN(String prodotti);
+    @Query(value = "SELECT * FROM ordine WHERE quantita > ?1", nativeQuery = true)
+    List<Ordine> cercaPerQuantitaMaggiore(int quantita);
 
-    @Query(value = "SELECT * FROM Ordine o WHERE o.importo = ?1", nativeQuery = true)
-    Ordine importoN(double importo);
+    @Query(value = "SELECT * FROM ordine WHERE id_users = ?1 ORDER BY dataCreazione DESC", nativeQuery = true)
+    List<Ordine> ultimiOrdini(Integer idUsers);
 
 }
