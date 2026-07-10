5cc78d99108f99592b9c260901201ab5b5a02e27
Vincenzo <Vincenzo_piccolo@outlook.it>
2026-07-10 11:51:36 +0200

    refactor: add OpenAPI annotations to AbstractController and UsersController for improved API documentation

--- src/main/java/it/classe/SpringClass/Controller/UsersController.java
+++ src/main/java/it/classe/SpringClass/Controller/UsersController.java
@@ -2,6 +2,7 @@ package it.classe.SpringClass.Controller;
 
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import it.classe.SpringClass.Dto.UsersDto;
 import it.classe.SpringClass.Service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,8 @@ public class UsersController extends AbstractController<UsersDto> {
     @Autowired
     private UsersService usersService;
 
-
+    @Operation(summary = "Recupera l'URL del database",
+            description = "Restituisce l'URL del database utilizzato dal servizio")
     @GetMapping("/url")
     public String url() {
         return usersService.getDatabaseUrl();
